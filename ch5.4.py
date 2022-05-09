4fbf49ba670e488626bae963c9849e29d1e1ab17
achange8 <95342381+achange8@users.noreply.github.com>
2022-05-09 13:48:18 +0900

    Update ch5.4.py

--- ch5.4.py
+++ ch5.4.py
@@ -1,7 +1,7 @@
 import scipy.cluster.hierarchy as hclst
 
 data = [[1, 2], [3, 1], [2, 3], [3, 6], [4, 6], [7, 2], [7, 4]]
-
+num = len(data)
 print("Data:")
 for no, d in enumerate(data):
     print("no=", no, "data=", d)
@@ -15,10 +15,10 @@ for res in results:
 cluster_tree = [x for x in range(len(data))]
 clusters = [x for x in range(len(data))]
 n = 0
-distance_max = 6
+distance_max = 2.5
 print("Clusters:")
 for a, b, distance, represent in results:
-    if distance < distance_max:
+    if distance <= distance_max:
         print("a=", int(a), "b=", int(b), distance, represent)
         if(int(a) >= len(data)):
             c = cluster_tree[int(a)]
@@ -32,6 +32,10 @@ for a, b, distance, represent in results:
         clusters.remove(c)
         clusters.remove(d)
         clusters.append((c, d))
+        if(distance < distance_max):
+            num += 1
         print(">>>> n=", n,  "number of clusters=",
               len(clusters), "clusters=", clusters)
         n += 1
+print(f"distance_max={distance_max}")
+print(f"直近のクラスタの番号は{num-1}です")
