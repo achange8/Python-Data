5707f6c4f56cb34f7c9980bc89d26f8a5814c53f
unknown <achange888@gmail.com>
2022-05-16 02:28:55 +0900

    up date code

--- ch5.5.py
+++ ch5.5.py
@@ -2,21 +2,23 @@ from secrets import choice
 import scipy.cluster.hierarchy as hclst
 
 data = [[1, 2], [3, 1], [2, 3], [3, 6], [4, 6], [7, 2], [7, 4]]
-num = len(data)
 print("Data:")
 for no, d in enumerate(data):
     print("no=", no, "data=", d)
 
 results = hclst.linkage(data, method='single', metric='euclidean')
-
 print("Results:")
 for res in results:
     print(res)
 
 cluster_tree = [x for x in range(len(data))]
 clusters = [x for x in range(len(data))]
+
+choice_no = 3
+other_no = 0
+num = no
 n = 0
-distance_max = 2.5
+distance_max = 6
 print("Clusters:")
 for a, b, distance, represent in results:
     if distance <= distance_max:
@@ -30,13 +32,18 @@ for a, b, distance, represent in results:
         else:
             d = int(b)
         cluster_tree.append((c, d))
+        if a == choice_no or b == choice_no:
+            num += n + 1
+            if a == choice_no:
+                other_no = int(b)
+            else:
+                other_no = int(a)
         clusters.remove(c)
         clusters.remove(d)
         clusters.append((c, d))
-        if(distance < distance_max):
-            num += 1
         print(">>>> n=", n,  "number of clusters=",
               len(clusters), "clusters=", clusters)
         n += 1
-print(f"distance_max={distance_max}")
-print(f"直近のクラスタの番号は{num-1}です")
+print(f"与えられたデータ={choice_no}番")
+print(f"そのクラスタの番号＝{num}")
+print(f"そのクラスタにあるほかのデータ＝{other_no}番")
--- ch5.6.py
+++ ch5.6.py
@@ -2,21 +2,23 @@ from secrets import choice
 import scipy.cluster.hierarchy as hclst
 
 data = [[1, 2], [3, 1], [2, 3], [3, 6], [4, 6], [7, 2], [7, 4]]
-num = len(data)
 print("Data:")
 for no, d in enumerate(data):
     print("no=", no, "data=", d)
 
 results = hclst.linkage(data, method='single', metric='euclidean')
-
 print("Results:")
 for res in results:
     print(res)
 
 cluster_tree = [x for x in range(len(data))]
 clusters = [x for x in range(len(data))]
+
+choice_no = 1
+parent_no = 0
+num = no
 n = 0
-distance_max = 2.5
+distance_max = 6
 print("Clusters:")
 for a, b, distance, represent in results:
     if distance <= distance_max:
@@ -29,14 +31,21 @@ for a, b, distance, represent in results:
             d = cluster_tree[int(b)]
         else:
             d = int(b)
+        if a == choice_no or b == choice_no:
+            num += n + 1
+
+        if a == num or b == num:
+            if a > b:
+                parent_no = int(a) + 1
+            else:
+                parent_no = int(b) + 1
         cluster_tree.append((c, d))
         clusters.remove(c)
         clusters.remove(d)
         clusters.append((c, d))
-        if(distance < distance_max):
-            num += 1
         print(">>>> n=", n,  "number of clusters=",
               len(clusters), "clusters=", clusters)
         n += 1
-print(f"distance_max={distance_max}")
-print(f"直近のクラスタの番号は{num-1}です")
+print(f"与えられたデータ={choice_no}番")
+print(f"そのクラスタの番号＝{num}")
+print(f"その上位のクラスタの番号＝{parent_no}番")
