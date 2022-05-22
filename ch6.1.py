512b272b12943a4561c3076e98aada1818b0f9b7
achange8 <95342381+achange8@users.noreply.github.com>
2022-05-23 00:45:56 +0900

    Update ch6.1.py

--- ch6.1.py
+++ ch6.1.py
@@ -1,13 +1,12 @@
-from cProfile import label
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import cluster
 
 # data
-data = [[1, 2], [3, 1], [2, 3], [3, 6], [4, 6], [7, 2], [7, 4]]
-pdata = np.array(data)
+data = [[1, 2], [3, 1], [2, 3], [3, 6], [4, 6], [7, 2], [7, 4], [5, 4]]
+data = np.array(data)
 
-model = cluster.KMeans(n_ckusters=3)
+model = cluster.KMeans(n_clusters=3)
 model.fit(data)
 labels = model.labels_
 
@@ -16,3 +15,11 @@ ldata = data[labels == 0]
 plt.scatter(ldata[:, 0], ldata[:, 1], marker='s', color='green')
 ldata = data[labels == 1]
 plt.scatter(ldata[:, 0], ldata[:, 1], marker='o', color='red')
+ldata = data[labels == 2]
+plt.scatter(ldata[:, 0], ldata[:, 1], marker='^', color='red')
+
+plt.title("Svatter Plot of Clustered Data")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.grid()
+plt.show()
