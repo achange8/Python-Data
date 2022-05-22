e7e7a57be5b03abf89fa55555cc52a3e29b77bb6
achange8 <95342381+achange8@users.noreply.github.com>
2022-05-23 01:19:19 +0900

    Update ch6.2.py

--- ch6.2.py
+++ ch6.2.py
@@ -1,24 +1,26 @@
-from turtle import distance
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import cluster
 
+# data
 data = [[1, 2], [3, 1], [2, 3], [3, 6], [4, 6], [7, 2], [7, 4], [5, 4], [5, 5]]
-K = 4
-pdata = np.array(data)
-N, L = np.shape(pdata)
-cdata = np.empty([K, L])
+data = np.array(data)
 
-cdata[0] = [(pdata[0, 0] + pdata[1, 0] + pdata[2, 0])/3.0,
-            (pdata[0, 1] + pdata[1, 1] + pdata[2, 1])/3.0]
-cdata[1] = [(pdata[5, 0] + pdata[6, 0])/2.0, (pdata[5, 1] + pdata[6, 1])/2.0]
-cdata[2] = [(pdata[3, 0] + pdata[4, 0])/2.0, (pdata[3, 1] + pdata[4, 1])/2.0]
+model = cluster.KMeans(n_clusters=4)
+model.fit(data)
+labels = model.labels_
 
-print("クラスタの中心点：")
-for k in range(K):
-    print(cdata[k])
-print("クラスタの中心から各データまでの距離：")
-for k in range(K):
-    for n in range(N):
-        distance = np.sqrt(
-            np.square(cdata[k, 0] - pdata[n, 0]) + np.square(cdata[k, 1]-pdata[n, 1]))
-        print("%10.6f" % distance, end=" ")
-    print()
+plt.figure(1)
+ldata = data[labels == 0]
+plt.scatter(ldata[:, 0], ldata[:, 1], marker='s', color='green')
+ldata = data[labels == 1]
+plt.scatter(ldata[:, 0], ldata[:, 1], marker='o', color='red')
+ldata = data[labels == 2]
+plt.scatter(ldata[:, 0], ldata[:, 1], marker='^', color='red')
+ldata = data[labels == 3]
+plt.scatter(ldata[:, 0], ldata[:, 1], marker='x', color='black')
+plt.title("Scatter Plot of Clustered Data")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.grid()
+plt.show()
