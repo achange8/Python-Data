e516b98a2d12cd8e4cf63a08aa548466cfc62fdb
unknown <achange888@gmail.com>
2022-05-23 00:18:29 +0900

    ?

--- ch6.1.py
+++ ch6.1.py
@@ -1,24 +1,18 @@
-from turtle import distance
+from cProfile import label
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import cluster
 
-data = [[1, 2], [3, 1], [2, 3], [3, 6], [4, 6], [7, 2], [7, 4], [5, 4]]
-K = 3
+# data
+data = [[1, 2], [3, 1], [2, 3], [3, 6], [4, 6], [7, 2], [7, 4]]
 pdata = np.array(data)
-N, L = np.shape(pdata)
-cdata = np.empty([K, L])
 
-cdata[0] = [(pdata[0, 0] + pdata[1, 0] + pdata[2, 0])/3.0,
-            (pdata[0, 1] + pdata[1, 1] + pdata[2, 1])/3.0]
-cdata[1] = [(pdata[5, 0] + pdata[6, 0])/2.0, (pdata[5, 1] + pdata[6, 1])/2.0]
-cdata[2] = [(pdata[3, 0] + pdata[4, 0])/2.0, (pdata[3, 1] + pdata[4, 1])/2.0]
+model = cluster.KMeans(n_ckusters=3)
+model.fit(data)
+labels = model.labels_
 
-print("クラスタの中心点：")
-for k in range(K):
-    print(cdata[k])
-print("クラスタの中心から各データまでの距離：")
-for k in range(K):
-    for n in range(N):
-        distance = np.sqrt(
-            np.square(cdata[k, 0] - pdata[n, 0]) + np.square(cdata[k, 1]-pdata[n, 1]))
-        print("%10.6f" % distance, end=" ")
-    print()
+plt.figure(1)
+ldata = data[labels == 0]
+plt.scatter(ldata[:, 0], ldata[:, 1], marker='s', color='green')
+ldata = data[labels == 1]
+plt.scatter(ldata[:, 0], ldata[:, 1], marker='o', color='red')
