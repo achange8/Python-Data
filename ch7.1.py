f039b4b53c0543be594352cffa7820f50401ad6e
achange8 <95342381+achange8@users.noreply.github.com>
2022-05-23 01:49:24 +0900

    update code

---
+++ ch7.1.py
@@ -0,0 +1,20 @@
+from pyexpat import model
+from sklearn import datasets
+from sklearn import model_selection
+from sklearn import naive_bayes
+from sklearn import metrics
+
+iris = datasets.load_iris()
+data_train, data_test, label_train, label_test = model_selection.train_test_split(
+    iris['data'], iris['target'], test_size=0.25)
+
+model = naive_bayes.GaussianNB().fit(data_train, label_train)
+
+y_true = label_test
+y_pred = model.predict(data_test)
+print("真ラベル =", y_true)
+print("予測ラベル =", y_pred)
+
+print("精度 =", metrics.accuracy_score(y_true, y_pred))
+print(metrics.classification_report(
+    y_true, y_pred, target_names=iris['target_names']))
