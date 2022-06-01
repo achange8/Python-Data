39b30c5eda0099068b783e4fcdcc45752f7012e5
achange8 <95342381+achange8@users.noreply.github.com>
2022-06-01 17:02:29 +0900

    Update ch8.2.py

--- ch8.2.py
+++ ch8.2.py
@@ -3,7 +3,7 @@ from sklearn import model_selection
 from sklearn import svm
 from sklearn import metrics
 
-#　データを取得
+
 size = [0.2, 0.4, 0.6, 0.8]
 for i in size:
     print("--------------------------------------------------------------")
@@ -12,19 +12,14 @@ for i in size:
     data_train, data_test, label_train, label_test = model_selection. train_test_split(
         iris['data'], iris['target'], test_size=i)
 
-    # モデルを作成
-    # ペナルティパラメータ
     C = 1000
-    # RBFカーネルのパラメータ
     gamma = 10
     model = svm.SVC(C=C, gamma=gamma)
     model.fit(data_train, label_train)
 
-    #　データを分類
     label_train_pred = model.predict(data_train)
     label_test_pred = model.predict(data_test)
 
-    # 分類結果の評価
     print("訓練データによる評価:")
     print("精度 =", metrics.accuracy_score(label_train, label_train_pred))
     print(metrics.classification_report(label_train,
