cfaa5cb4427941ca1cfcec910dd3933ddd300bf3
unknown <achange888@gmail.com>
2022-06-06 01:33:55 +0900

    Update ch8.3.py

--- ch8.3.py
+++ ch8.3.py
@@ -3,27 +3,25 @@ from sklearn import model_selection
 from sklearn import svm
 from sklearn import metrics
 
-#　データを取得
+
 Clist = [1, 10, 100, 1000]
 for i in range(len(Clist)):
+    print("--------------------------------------------------------------")
+
     print("size = ", Clist[i])
     iris = datasets.load_iris()
     data_train, data_test, label_train, label_test = model_selection. train_test_split(
         iris['data'], iris['target'], test_size=0.25)
 
-    # モデルを作成
-    # ペナルティパラメータ
     C = Clist[i]
-    # RBFカーネルのパラメータ
+
     gamma = 10
     model = svm.SVC(C=C, gamma=gamma)
     model.fit(data_train, label_train)
 
-    #　データを分類
     label_train_pred = model.predict(data_train)
     label_test_pred = model.predict(data_test)
 
-    # 分類結果の評価
     print("訓練データによる評価:")
     print("精度 =", metrics.accuracy_score(label_train, label_train_pred))
     print(metrics.classification_report(label_train,
